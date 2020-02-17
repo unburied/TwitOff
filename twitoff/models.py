447e543bef9310548eac553d99f670d4b072c4da
David <davi86m@gmail.com>
2020-02-16 22:45:02 -0500

    Added formatting

--- twitoff/models.py
+++ twitoff/models.py
@@ -9,11 +9,16 @@ class User(DB.Model):
 
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return f'<User {self.name}>'
 
 class Tweet(DB.Model):
     """Tweest we pull"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-    
+    user = DB.relationship('User', backref=DB.backref('tweets'), lazy=True)
+    def __repr__(self):
+        return f'<Tweet {self.text}>'
 
