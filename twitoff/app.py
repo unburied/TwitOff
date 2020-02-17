aa6482a2a9145eb25ad357a7c08656a60380cbc0
David <davi86m@gmail.com>
2020-02-16 23:54:37 -0500

    Added twitter.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,21 +1,24 @@
 """Twitoff app to analyze and compare tweets"""
 
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User 
 # create app factory
 
 def create_app():
     app = Flask(__name__)
 
     # Database config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'config['DATABASE_URL']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # link Database to App
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
 
     return app
 
