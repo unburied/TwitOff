89abcfdfbf7a288b282840716fe78f40e5eacacf
David <davi86m@gmail.com>
2020-02-16 21:31:44 -0500

    Removed files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,14 +1,21 @@
 """Twitoff app to analyze and compare tweets"""
 
 from flask import Flask 
-
+from .models import DB
 # create app factory
 
 def create_app():
     app = Flask(__name__)
 
+    # Database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    # link Database to App
+    DB.init_app(app)
+    
     @app.route('/')
     def root():
         return 'Welcome to Twitoff'
 
     return app
+
